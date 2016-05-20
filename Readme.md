d9bd76641a43094d772235af100eb4754a23461b
Mark Vayngrib <mark@tradle.io>
2016-05-20 18:43:43 -0400

    reorg, custom type support

--- Readme.md
+++ Readme.md
@@ -5,7 +5,63 @@ Type-accurate serialization of javascript objects
 
 [![build status](https://secure.travis-ci.org/carlos8f/hydration.png)](http://travis-ci.org/carlos8f/hydration)
 
-@todo usage + examples
+## Usage
+
+### Basic
+
+out-of-the-box support for Date, RegExp, arrays, buffers
+
+```js
+var hydra = require('hydration')
+var obj = {
+  date: new Date(),
+  regex: /^something/,
+  array: ['of', 747]
+}
+
+// basic
+
+var dehydrated = hydra.dehydrate(obj)
+// JSON.stringify + JSON.parse if you like
+var hydrated = hydra.hydrate(dehydrated)
+
+// hydrated deepEquals obj
+```
+
+### Advanced
+
+you can add support for custom types yourself
+
+```js
+var hydra = require('hydration')
+
+function OogaBooga (name) {
+  this.name = name
+}
+
+var custom = hydra()
+custom.addType('oogabooga', {
+  test: function (val) {
+    return val instanceof OogaBooga
+  },
+  dehydrate: function (val) {
+    return val.name
+  },
+  hydrate: function (val) {
+    return new OogaBooga(val)
+  }
+})
+
+var obj = {
+  //...
+  someprop: new OogaBooga('yum')
+  //...
+}
+
+var dehydrated = hydra.dehydrate(obj)
+// JSON.stringify + JSON.parse if you like
+var hydrated = hydra.hydrate(dehydrated)
+```
 
 ## Thanks
 
--- index.js
+++ index.js
@@ -1,142 +1,225 @@
-function dehydrate(input) {
-  if (isPrimitiveOrNull(input)) return input;
 
-  var obj = {}, k, typeCount = 0;
-  for (k in input) {
-    if (!input.hasOwnProperty(k)) continue;
-    obj[k] = input[k];
+var builtInTypes = {
+  string: {
+    test: function (val) {
+      return typeof val === 'string'
+    },
+    dehydrate: identity,
+    hydrate: identity
+  },
+  'null': {
+    test: function (val) {
+      return val == null
+    },
+    hydrate: function () {
+      return null
+    },
+    dehydrate: function () {
+      return ''
+    }
+  },
+  number: {
+    test: function (val) {
+      return typeof val === 'number'
+    },
+    hydrate: function (val) {
+      return parseFloat(val)
+    },
+    dehydrate: toString
+  },
+  date: {
+    test: function (val, str) {
+      return str === '[object Date]'
+    },
+    hydrate: function (val) {
+      return new Date(parseInt(val))
+    },
+    dehydrate: function (val) {
+      return +val
+    }
+  },
+  boolean: {
+    test: function (val) {
+      return typeof val === 'boolean'
+    },
+    hydrate: function (val) {
+      return val === 'false' || val === '0' ? false : true
+    },
+    dehydrate: toString
+  },
+  regexp: {
+    test: function (val, str) {
+      return str === '[object RegExp]'
+    },
+    hydrate: function (val) {
+      return new RegExp(val.replace(/(^\/|\/$)/g, ''))
+    },
+    dehydrate: toString
+  },
+  buffer: {
+    test: function (val) {
+      return Buffer.isBuffer(val)
+    },
+    hydrate: function (val) {
+      return new Buffer(val, 'base64')
+    },
+    dehydrate: function (val) {
+      return val.toString('base64')
+    }
+  },
+  array: {
+    test: function (val) {
+      return Array.isArray(val)
+    },
+    dehydrate: function (arr, hydra) {
+      // Convert to an object so we can store types.
+      var newObj = {}
+      for (var i = 0; i < arr.length; i++) {
+        newObj[i] = arr[i]
+      }
+
+      return hydra.dehydrate(newObj, hydra)
+    },
+    hydrate: function (val, hydra) {
+      val = hydra.hydrate(val)
+
+      // Convert back to array.
+      var newArr = new Array(Object.keys(val).length)
+      for (var i in val) {
+        newArr[i] = val[i]
+      }
+
+      return newArr
+    }
+  },
+  object: {
+    type: function (val) {
+      return val && typeof val === 'object'
+    },
+    hydrate: function (val, hydra) {
+      return hydra.hydrate(val)
+    },
+    dehydrate: function (val, hydra) {
+      return hydra.dehydrate(val)
+    }
   }
-  obj.__proto__ = input.__proto__; // so prototype.toJSON() will still fire
-  obj._types = {};
-
-  for (k in obj) {
-    if (!obj.hasOwnProperty(k)) continue;
-    if (k == '_types') {
-      continue
-    }
-    var type = getType(obj[k]);
-    // Strings are implied
-    if (type !== 'string') {
-      obj._types[k] = type;
-      typeCount++;
-    }
-    switch (type) {
-      case 'array':
-        // Convert to an object so we can store types.
-        var newObj = {};
-        for (var i in obj[k]) {
-          newObj[i] = obj[k][i];
-        }
-        obj[k] = newObj;
-        obj[k] = dehydrate(obj[k]);
-        break;
-      case 'regexp':
-        obj[k] = obj[k].toString();
-        break;
-      case 'object':
-        obj[k] = dehydrate(obj[k]);
-        break;
-      case 'buffer':
-        obj[k] = obj[k].toString('base64');
-        break;
-    }
-  }
-
-  if (typeCount === 0) {
-    delete obj._types;
-  }
-  return obj;
-};
-exports.dehydrate = dehydrate;
-
-function getType(obj) {
-  if (typeof obj === 'object') {
-    var str = Object.prototype.toString.call(obj);
-    if (str === '[object Array]') {
-      return 'array';
-    }
-    else if (str === '[object RegExp]') {
-      return 'regexp';
-    }
-    else if (str === '[object Date]') {
-      return 'date';
-    }
-    else if (str === '[object Null]') {
-      return 'null';
-    }
-    else if (typeof Buffer !== 'undefined' && Buffer.isBuffer(obj)) {
-      return 'buffer';
-    }
-
-    return 'object';
-  }
-  return typeof obj;
 }
 
-function hydrate(input) {
-  if (isPrimitiveOrNull(input)) return input;
+function identity (val) {
+  return val
+}
 
-  var obj = {}, k;
-  for (k in input) {
-    if (!input.hasOwnProperty(k)) continue;
-    obj[k] = input[k];
+function toString (val) {
+  return val.toString ? val.toString() : val
+}
+
+function hydra (opts) {
+  var byType = {}
+  for (var name in builtInTypes) {
+    byType[name] = builtInTypes[name]
   }
-  if (!obj._types) {
-    return obj;
+
+  var api = {
+    hydrate: hydrate,
+    dehydrate: dehydrate,
+    addType: addType
   }
-  for (k in obj) {
-    if (!obj.hasOwnProperty(k)) continue;
-    if (k == '_types') {
-      continue;
+
+  return api
+
+  function dehydrate (input) {
+    if (isPrimitiveOrNull(input)) return input
+
+    var obj = {}, k, typeCount = 0
+    obj.__proto__ = input.__proto__ // so prototype.toJSON() will still fire
+    obj._types = {}
+
+    for (k in input) {
+      if (!input.hasOwnProperty(k)) continue
+
+      var val = input[k]
+      var name = getType(val)
+      if (!name) throw new Error('missing dehydrator for property: ' + k)
+
+      typeCount++
+      var hd = byType[name]
+      obj._types[k] = name
+      obj[k] = hd.dehydrate(val, api)
     }
-    switch (obj._types[k]) {
-      case 'boolean':
-        if (typeof obj[k] === 'string') {
-          if (obj[k] === 'false' || obj[k] === '0') {
-            obj[k] = false;
-          }
-          else {
-            obj[k] = true;
-          }
-        }
-        break;
-      case 'date':
-        if (typeof obj[k] == 'string') {
-          obj[k] = new Date(obj[k]);
-        }
-        break;
-      case 'regexp':
-        obj[k] = new RegExp(obj[k].replace(/(^\/|\/$)/g, ''));
-        break;
-      case 'number':
-        obj[k] = parseFloat(obj[k]);
-        break;
-      case 'array':
-        obj[k] = hydrate(obj[k]);
-        // Convert back to array.
-        var newArr = [];
-        for (var i in obj[k]) {
-          newArr.push(obj[k][i]);
-        }
-        obj[k] = newArr;
-        break;
-      case 'object':
-        obj[k] = hydrate(obj[k]);
-        break;
-      case 'buffer':
-        obj[k] = new Buffer(obj[k], 'base64')
-        break;
-      case 'null':
-        obj[k] = null;
-        break;
+
+    if (typeCount === 0) {
+      delete obj._types
     }
+
+    return obj
   }
-  delete obj._types;
-  return obj;
+
+  function hydrate (input) {
+    if (isPrimitiveOrNull(input)) return input
+    if (!input._types) return input
+
+    var obj = {}, k
+    for (k in input) {
+      if (!input.hasOwnProperty(k)) continue
+      if (k == '_types') {
+        continue
+      }
+
+      var name = input._types[k]
+      var hd = byType[name]
+      if (!hd) throw new Error('missing hydrator for type: ' + k)
+
+      obj[k] = hd.hydrate(input[k], api)
+    }
+
+    delete obj._types
+    return obj
+  }
+
+  function getType (val) {
+    var str = Object.prototype.toString.call(val)
+    for (var name in byType) {
+      if (name === 'object') continue
+      if (byType[name].test(val, str)) return name
+    }
+
+    // fallback to object
+    return typeof val === 'object' ? 'object' : null
+  }
+
+  function addType (name, opts) {
+    assert(typeof opts === 'object', 'expected "opts"')
+    assert(typeof opts.test === 'function', 'expected "test"')
+    assert(typeof opts.hydrate === 'function', 'expected "hydrate"')
+    assert(typeof opts.dehydrate === 'function', 'expected "dehydrate"')
+
+    if (name in byType) {
+      throw new Error('type "' + name + '" already exists!')
+    }
+
+    // defensive copy
+    byType[name] = {
+      test: opts.test,
+      hydrate: opts.hydrate,
+      dehydrate: opts.dehydrate
+    }
+
+    return api
+  }
 }
 
 function isPrimitiveOrNull (input) {
   return input == null || typeof input !== 'object'
 }
 
-exports.hydrate = hydrate;
+function assert (statement, msg) {
+  if (!statement) throw new Error(msg)
+}
+
+
+module.exports = hydra
+
+// backwards compatibility
+var builtIn = hydra()
+module.exports.hydrate = builtIn.hydrate
+module.exports.dehydrate = builtIn.dehydrate
